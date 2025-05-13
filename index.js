cce2287f9e31195cd0870bc37a55f8a7623ffea3
Edward <edward.nayve@boom.ai>
2025-05-13 18:34:08 +0800

    Add encryption on user location data

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const CryptoJS = require("crypto-js");
 const LocationModel = require('./models/location')
 
 // Load environment variables
@@ -19,8 +20,21 @@ const io = new Server(server, {
     }
 });
 
+
 // MongoDB connection
 const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/socketio_app';
+const SECRET_KEY = process.env.SECRET_KEY || 'development';
+
+// Utility function
+const encryptField = (value) => {
+    return CryptoJS.AES.encrypt(value.toString(), SECRET_KEY).toString();
+  };
+
+
+const decrypt = (value) => {
+    const bytes = CryptoJS.AES.decrypt(value, SECRET_KEY)
+    return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+}
 
 mongoose.connect(MONGODB_URL)
     .then(() => console.log('MongoDB connected'))
@@ -38,15 +52,21 @@ io.on('connection', (socket) => {
 
 
     socket.on('update_location', async (locationData) => {
-        const { userId } = locationData
+        const { userId, latitude, longitude } = locationData
         
+        const cipheredData = {
+            ...locationData,
+            latitude: encryptField(latitude),
+            longitude: encryptField(longitude),
+        }
+
+        console.log(cipheredData)
         await LocationModel.findOneAndUpdate(
             { userId },
-            locationData,
+            cipheredData,
             { upsert: true }
         )
-
-        socket.broadcast.emit('location_update', locationData)
+        socket.broadcast.emit('location_update', cipheredData)
     })
 
 
