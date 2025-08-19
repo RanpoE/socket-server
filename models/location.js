75ad8760e5365d19753427fae90fa08a44b238fb
Edward <edward.nayve@boom.ai>
2025-08-19 20:54:59 +0800

    Update location schema

--- models/location.js
+++ models/location.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 const LocationSchema = new mongoose.Schema({
-    userId: String,
-    latitude: String,
-    longitude: String,
-    timestamp: Date
-  });
-  
-  const LocationModel = mongoose.model('Location', LocationSchema);
-  
+  userId: String,
+  latitude: String,
+  longitude: String,
+  timestamp: Date,
+  plate: { type: String }
+});
+
+const LocationModel = mongoose.model('Location', LocationSchema);
+
 module.exports = LocationModel
